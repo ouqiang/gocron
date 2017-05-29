49e2c15473a82a107eb243187677a1616fdcc929
ouqiang <qingqianludao@gmail.com>
2017-05-29 21:32:22 +0800

    数据库表添加索引

--- models/task_log.go
+++ models/task_log.go
@@ -14,14 +14,14 @@ type TaskLog struct {
     TaskId   int       `xorm:"int notnull index default 0"`       // 任务id
     Name     string    `xorm:"varchar(32) notnull"`               // 任务名称
     Spec     string    `xorm:"varchar(64) notnull"`               // crontab
-    Protocol TaskProtocol  `xorm:"tinyint notnull"`               // 协议 1:http 2:RPC
+    Protocol TaskProtocol  `xorm:"tinyint notnull index"`               // 协议 1:http 2:RPC
     Command  string    `xorm:"varchar(256) notnull"`              // URL地址或shell命令
     Timeout  int       `xorm:"mediumint notnull default 0"`       // 任务执行超时时间(单位秒),0不限制
     RetryTimes int8    `xorm:"tinyint notnull default 0"`           // 任务重试次数
     Hostname string       `xorm:"varchar(128) notnull defalut '' "`   // RPC主机名，逗号分隔
     StartTime time.Time `xorm:"datetime created"`                   // 开始执行时间
     EndTime   time.Time `xorm:"datetime updated"`                   // 执行完成（失败）时间
-    Status    Status    `xorm:"tinyint notnull default 1"`          // 状态 0:执行失败 1:执行中  2:执行完毕 3:任务取消(上次任务未执行完成) 4:异步执行
+    Status    Status    `xorm:"tinyint notnull index default 1"`          // 状态 0:执行失败 1:执行中  2:执行完毕 3:任务取消(上次任务未执行完成) 4:异步执行
     Result    string    `xorm:"mediumtext notnull defalut '' "` // 执行结果
     TotalTime int       `xorm:"-"` // 执行总时长
     BaseModel   `xorm:"-"`
