370645fca8422e30113fc7f5da3022f1bcde0731
zhmin <myzhmin@gmail.com>
2018-07-11 17:50:20 +0800

    fix check database if exist

--- internal/models/migration.go
+++ internal/models/migration.go
@@ -13,9 +13,6 @@ type Migration struct{}
 
 // 首次安装, 创建数据库表
 func (migration *Migration) Install(dbName string) error {
-	if !isDatabaseExist(dbName) {
-		return errors.New("数据库不存在")
-	}
 	setting := new(Setting)
 	task := new(Task)
 	tables := []interface{}{
@@ -37,13 +34,6 @@ func (migration *Migration) Install(dbName string) error {
 	setting.InitBasicField()
 
 	return nil
-}
-
-// 判断数据库是否存在
-func isDatabaseExist(name string) bool {
-	_, err := Db.Exec("use ?", name)
-
-	return err != nil
 }
 
 // 迭代升级数据库, 新建表、新增字段等
