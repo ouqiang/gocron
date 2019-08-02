6eaa5f3fc2a0076e47004fa0b8882df6209a7c3f
luobangkui <luobangkui@nequal.com>
2019-08-02 21:29:17 +0800

    节点配置多了会报错hostname column too lang , 最好把这个值调大

--- internal/models/task_log.go
+++ internal/models/task_log.go
@@ -18,7 +18,7 @@ type TaskLog struct {
 	Command    string       `json:"command" xorm:"varchar(256) notnull"`              // URL地址或shell命令
 	Timeout    int          `json:"timeout" xorm:"mediumint notnull default 0"`       // 任务执行超时时间(单位秒),0不限制
 	RetryTimes int8         `json:"retry_times" xorm:"tinyint notnull default 0"`     // 任务重试次数
-	Hostname   string       `json:"hostname" xorm:"varchar(128) notnull default '' "` // RPC主机名，逗号分隔
+	Hostname   string       `json:"hostname" xorm:"varchar(1024) notnull default '' "` // RPC主机名，逗号分隔
 	StartTime  time.Time    `json:"start_time" xorm:"datetime created"`               // 开始执行时间
 	EndTime    time.Time    `json:"end_time" xorm:"datetime updated"`                 // 执行完成（失败）时间
 	Status     Status       `json:"status" xorm:"tinyint notnull index default 1"`    // 状态 0:执行失败 1:执行中  2:执行完毕 3:任务取消(上次任务未执行完成) 4:异步执行
