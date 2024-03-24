d01fb93ab8394288ff077fada0d3362aa4d2287b
codeyu <codeyu@gmail.com>
2024-03-24 18:32:37 +0900

    up

--- web/vue/build/webpack.base.conf.js
+++ web/vue/build/webpack.base.conf.js
@@ -72,7 +72,8 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          publicPath: '../../'
         }
       }
     ]
