5658b7cc81e0331174c7e5317b675fb032de530b
codeyu <codeyu@gmail.com>
2024-03-24 19:57:40 +0900

    up

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 # Copy service file, incase if there are any changes
 cd ~/gocron
+sudo cp bin/* /gocron/
 sudo cp gocron-web.service /etc/systemd/system/gocron-web.service
 sudo cp gocron-node.service /etc/systemd/system/gocron-node.service
 # reload configurations incase if service file has changed
